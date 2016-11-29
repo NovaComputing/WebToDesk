33bf2e2d6ac0079a5bbc73fbbec56295e6b317c0
spit4520 <spit4520@kettering.edu>
2016-11-29 11:00:32 -0500

    Add files via upload

--- WebToDesk/install.sh
+++ WebToDesk/install.sh
@@ -5,11 +5,11 @@ pip install selenium
 
 sudo mkdir /opt/WebToDesk/
 sudo mkdir /opt/WebToDesk/bin/
-sudo mkdir /opt/WebToDesk/bashAssets/
+sudo mkdir /opt/WebToDesk/assets/
 
 sudo chmod o+r+w /opt/WebViewer/*
-sudo cp ~/Downloads/WebToDesk/Python /opt/WebToDesk/bin/
-sudo cp ~/Downloads/WebToDesk/etc/WebViewer.txt /etc/WebToDesk.txt
-sudo cp ~/Downloads/WebToDesk/scripts/* /opt/WebToDesk/bin/
+sudo cp ~/Downloads/WebToDesk/Python/WebToDesk.py /opt/WebToDesk/bin/
+sudo cp ~/Downloads/WebToDesk/etc/WebViewer.txt /etc/
+sudo cp ~/Downloads/WebToDesk/scripts/* /usr/bin/
 sudo cp ~/Downloads/WebToDesk/Assets/* /opt/WebToDesk/assets/
 sudo cp ~/Downloads/WebToDesk/iconFiles/* ~/Desktop/
